596ac0b1ae179f38690df35af2f219605db54473
kerktaiheng <taihengkerk@gmail.com>
2025-01-23 22:08:41 +0800

    Rename chatbot to Alice, greet and exit

---
+++ src/main/java/Alice.java
@@ -0,0 +1,21 @@
+public class Alice {
+    public static void main(String[] args) {
+        String logo = 
+            "   ,---,        ,--,                                 \n" +
+            "  '  .' \\     ,--.'|     ,--,                        \n" +
+            " /  ;    '.   |  | :   ,--.'|                        \n" +
+            ":  :       \\  :  : '   |  |,                         \n" +
+            ":  |   /\\   \\ |  ' |   `--'_       ,---.     ,---.   \n" +
+            "|  :  ' ;.   :'  | |   ,' ,'|     /     \\   /     \\  \n" +
+            "|  |  ;/  \\   \\  | :   '  | |    /    / '  /    /  | \n" +
+            "'  :  | \\  \\ ,'  : |__ |  | :   .    ' /  .    ' / | \n" +
+            "|  |  '  '--' |  | '.'|'  : |__ '   ; :__ '   ;   /| \n" +
+            "|  :  :       ;  :    ;|  | '.'|'   | '.'|'   |  / | \n" +
+            "|  | ,'       |  ,   / ;  :    ;|   :    :|   :    | \n" +
+            "`--''          ---`-'  |  ,   /  \\   \\  /  \\   \\  /  \n" +
+            "                        ---`-'    `----'    `----'   \n";
+        System.out.println("Hello from\n\n" + logo + "\n\n");
+        System.out.println("What can I do for you today?\n\n");
+        System.out.println("See you soon!");
+    }
+}
