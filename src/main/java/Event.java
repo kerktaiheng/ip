ed2f101e5205d902bdabaa518c3510353ff0863b
kerktaiheng <taihengkerk@gmail.com>
2025-01-29 14:57:03 +0800

    Add Task Saving and Loading

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -8,8 +8,20 @@ public class Event extends Task{
         this.to = to;
     }
 
+    public Event(String name, String from, String to, boolean isMarked) {
+        super(name, isMarked);
+        this.from = from;
+        this.to = to;
+    }
+
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
+
+    @Override
+    public String toDataString() {
+        return "E|" + (isMarked ? "1" : "0") + "|" + name + "|" + from + "|" + to;
+    }
+    
 }
